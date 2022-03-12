f97f7a593f472b22814d1b0cd6eff3447e2d2c2a
shakhawat-khan <mkhan172175@bscse.uiu.ac.bd>
2022-03-12 22:45:48 +0600

    button

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button,Alert } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      
+      <Text>Shakhawat Rocks !!</Text>
+      <Text style={{color:'green'}}>React Native is good and i am comming for you </Text>
+
+      <Text style = {styles1.bigBlue}  > this is a new line  </Text>
+      
+      <Text style ={styles1.darkblue} > this is another new line </Text>
+
+      <View >
+      <Button 
+      title={'React Native Elements'}
+      onPress={() => Alert.alert('Simple Button pressed')}
+      color="#f194ff"
+      
+      />
+      </View>
+
       <StatusBar style="auto" />
     </View>
+
+
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +39,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+const styles1 = StyleSheet.create(
+  {
+    bigBlue :{
+      color: 'blue',
+      fontSize: 30,
+      fontWeight: 'bold',
+    },
+    
+    darkblue:{
+      color: 'red',
+      fontSize: 25,
+      fontWeight: '300',
+      fontStyle: 'italic',
+    }
+
+  }
+);
