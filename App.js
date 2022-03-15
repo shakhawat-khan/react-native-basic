408bcc26e69df0a49e4bc60e36b29f148ff6ced9
shakhawat-khan <mkhan172175@bscse.uiu.ac.bd>
2022-03-15 17:21:40 +0600

    map and scrollview

--- App.js
+++ App.js
@@ -1,100 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View,Button,Alert,TextInput } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Button,
+  Alert,
+  TextInput,
+  Text,
+  ScrollView,
+} from "react-native";
 
-
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function App() {
-
-  const [name1,setName1] = useState('');
-  const [name2,setName2] = useState('');
-  const [temp,setTemp] = useState('');
-  const [person,setPerson] = useState({name:'mario',age:40});
-
-  const clickHandler = () =>
-  {
-    setTemp(parseFloat(name1) + parseFloat(name2) )
-  }
-
-
+  const [people, setPeople] = useState([
+    { name: "sajjad", key: 1 },
+    { name: "sristy", key: 2 },
+    { name: "isty", key: 3 },
+    { name: "shomonnoy", key: 4 },
+    { name: "rifa", key: 5 },
+    { name: "barsha", key: 6 },
+    { name: "emum", key: 7 },
+  ]);
 
   return (
-    <View style={styles.container}>
-  
-      <TextInput
-      style = {styles.input}
-      placeholder='input'
-      keyboardType= 'numeric'
-      onChangeText={(val) => setName1(val) } 
-      />
-
-        
-      <TextInput
-      style = {styles.input}
-      placeholder='input'
-      keyboardType= 'numeric'
-      onChangeText={(val) => setName2(val) } 
-      />
-
-      <View>
-        <Button
-
-        title='add'
-        
-        onPress={ clickHandler }
-
-        />
-      </View>
-
-      <View>
-        <Text> {temp} </Text>
-      </View>
-
-      
-
-      <StatusBar style="auto" />
+    <View>
+      <ScrollView>
+        {people.map((item) => {
+          return (
+            <View key={item.key}>
+              <Text style={styles.Item}>{item.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
-
-
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
-  input :{
+  input: {
     borderWidth: 3,
-    borderColor: '#000000',
+    borderColor: "#000000",
     padding: 8,
     margin: 5,
-  }
+  },
 
-  
-
+  Item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
+  },
 });
 
+const styles1 = StyleSheet.create({
+  bigBlue: {
+    color: "blue",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
 
-const styles1 = StyleSheet.create(
-  {
-    bigBlue :{
-      color: 'blue',
-      fontSize: 30,
-      fontWeight: 'bold',
-    },
-    
-    darkblue:{
-      color: 'red',
-      fontSize: 25,
-      fontWeight: '300',
-      fontStyle: 'italic',
-    }
-
-  }
-);
+  darkblue: {
+    color: "red",
+    fontSize: 25,
+    fontWeight: "300",
+    fontStyle: "italic",
+  },
+});
